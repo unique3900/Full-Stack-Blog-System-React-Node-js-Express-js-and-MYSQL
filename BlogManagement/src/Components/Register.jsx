e2d88d57903746eb36ec8e7c3cd14fcd83fa0cc8
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-05-30 16:10:08 +0545

    user registered successfully from frontend form

--- BlogManagement/src/Components/Register.jsx
+++ BlogManagement/src/Components/Register.jsx
@@ -1,42 +1,90 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, {
+    useState
+} from 'react';
+
+
 
 const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const handleSubmit = (e) => {
+    const [name, setName] = useState("");
+    const handleSubmit = async(e) => {
         e.preventDefault();
-        console.log(email + password);
+        try {
+            const { data } = await axios.post('http://localhost:8080/api/v1/auth/register', { name, email, password });
+            console.log(data);
+         
+
+        } catch (error) {
+            console.log("Internal Server Error in client" + error);
+        }
     }
-  return (
-    <div className='h-screen flex flex-col justify-center items-center '>
-    <div className="h-96 w-96 bg-slate-100 p-5 shadow-lg">
-        <form action="">
-        <h1 className="text-center text-3xl font-bold">Register</h1>
-            <div className="flex flex-col justify-center mt-3 align-middle">
-                {/* inputBox */}
-                <div className="flex flex-col gap-2 w-full  py-2">
-                    <label htmlFor="">Email</label>
-                    <input value={email} onChange={(e) => {
-                        setEmail(e.target.value)
-                    }} type="email" className='rounded-xl outline-black w-full px-3 py-2 ' name="" id="" placeholder='Enter Email' />
-                </div>
+    return (
+        <div className='h-screen flex flex-col justify-center items-center '>
+            <div className="h-96 w-96 bg-slate-100 p-5 shadow-lg">
+                <form action="" onSubmit={handleSubmit}>
+                    <h1 className="text-center text-3xl font-bold">Register</h1>
+                    <div className="flex flex-col justify-center mt-3 align-middle">
+                        {/* inputBox */}
+
+                        <div className="flex flex-col gap-2 w-full  py-2">
+                            <label htmlFor="">Full Name</label>
+                            <input value={name}
+                                onChange={
+                                    (e) => {
+                                        setName(e.target.value)
+                                    }
+                                }
+                                type="text"
+                                className='rounded-xl outline-black w-full px-3 py-2 '
+                                name=""
+                                id=""
+                                placeholder='Enter Full Name' required/>
+                        </div>
+                    <div className="flex flex-col gap-2 w-full  py-2">
+                        <label htmlFor="">Email</label>
+                        <input value={email}
+                            onChange={
+                                (e) => {
+                                    setEmail(e.target.value)
+                                }
+                            }
+                            type="email"
+                            className='rounded-xl outline-black w-full px-3 py-2 '
+                            name=""
+                            id=""
+                            placeholder='Enter Email' required/>
+                    </div>
 
                 <div className="flex flex-col gap-2 w-full  py-2">
                     <label htmlFor="">Password</label>
-                    <input value={password} onChange={(e) => {
-                        setPassword(e.target.value)
-                    }} type="password" className='rounded-xl outline-black w-full px-3 py-2 ' name="" id="" placeholder='Enter Password' />
+                    <input value={password}
+                        onChange={
+                            (e) => {
+                                setPassword(e.target.value)
+                            }
+                        }
+                        type="password"
+                        className='rounded-xl outline-black w-full px-3 py-2 '
+                        name=""
+                        id=""
+                        placeholder='Enter Password' required/>
                 </div>
 
-                <div className="flex flex-col gap-2 w-full  py-2">
-                    <input type="submit" value={"Register"} className='w-full bg-green-600 text-white px-3 py-4 rounded-xl ' name="" id="" onClick={handleSubmit}  />
-                </div>
+            <div className="flex flex-col gap-2 w-full  py-2">
+                <input type="submit"
+                    value={"Register"}
+                    className='w-full cursor-pointer bg-green-600 text-white px-3 py-4 rounded-xl '
+                    name=""
+                    id=""
+                   />
             </div>
-        </form>
-        
-</div>
-</div>
-  )
+        </div>
+    </form>
+
+</div></div>
+    )
 }
 
 export default Register
--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
 const { connection } = require('./DB/conn');
 
 const authRoutes=require('./routes/authRoutes.js');
 
 app.use(express.json({limit : 52428800}));
+app.use(morgan('dev'));
+app.use('/api/v1/auth', authRoutes);
 
-app.use('/api/v1/auth',authRoutes);
+
+app.use(cors());
 
 
 const port = process.env.PORT;
